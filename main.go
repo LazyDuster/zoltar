a515c0a7f91cff9db586cef0294d475949fece3b
Michal Lesniewski <michalklesniewski@gmail.com>
2021-01-26 14:35:36 -0500

    Added owoify

--- main.go
+++ main.go
@@ -6,13 +6,38 @@ import (
 	"math/rand"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 	"github.com/bwmarrin/discordgo"
 )
 
+const (
+	words = 5
+	commands = 5
+)
+
 var fortunes []string
 var offensive []string
+var commands = []string{"!fortune", "!offendme", "!owoify", "!cloaker", "!fuckme"}
+var funcs = []func(){}
+
+/* lol */
+func owoify(sentence string) (out string) {
+	faces := []string{"owo", "OWO", "OwO", "UwU", ">w<", "^w^", "uwu"}
+	glyphs := [words][2]string{
+		{"l", "w"},
+		{"r", "w"},
+		{"L", "W"},
+		{"R", "W"},
+		{"!", faces[rand.Intn(6)]},
+	}
+	out = strings.ReplaceAll(sentence, glyphs[0][0], glyphs[0][1])
+	for i := 1; i < words; i++ {
+		out = strings.ReplaceAll(out, glyphs[i][0], glyphs[i][1])
+	}
+	return out
+}
 
 /* Scanner split function, reads in file and delimits by % */
 func FortuneSplit(data []byte, atEOF bool) (advance int, token []byte, err error) {
@@ -56,18 +81,40 @@ func SendFortune(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if m.Content == "!fortune" {
+	cmdstr := strings.SplitN(m.Content, " ", 2)
+
+	if cmdstr[0] == "!fortune" {
 		msgtitle := m.Author.Username + ", it's your lucky day."
 		me := discordgo.MessageEmbed{ Title: msgtitle, Description: GetFortune(), Color: 39423 }
 		s.ChannelMessageSendEmbed(m.ChannelID, &me)
-	} else if m.Content == "!offendme" {
+	} else if cmdstr[0] == "!offendme" {
 		msgtitle := m.Author.Username + ", it's your lucky day."
 		me := discordgo.MessageEmbed{ Title: msgtitle, Description: GetFortune(), Color: 14103594 }
 		s.ChannelMessageSendEmbed(m.ChannelID, &me)
-	} else if m.Content == "!zoltar" {
+	} else if cmdstr[0] == "!owoify" {
+		msgtitle := m.Author.Username + ", are you fucking kidding me?"
+		me := discordgo.MessageEmbed{ Title: msgtitle, Description: owoify(cmdstr[1]), Color: 0xFFC0CB }
+		s.ChannelMessageSendEmbed(m.ChannelID, &me)
+	} else if cmdstr[0] == "!fuckme" {
+		me := discordgo.MessageEmbed{ Title: "An odd request...", Description: "...but okay, \\*cums\\*.", Color: 0xFFC0CB }
+		s.ChannelMessageSendEmbed(m.ChannelID, &me)
+	} else if cmdstr[0] == "!fuckmeinitalian" {
+		me := discordgo.MessageEmbed{ Title: "A spicy meatball...", Description: "...but okay, *cums*.", Color: 0xFFC0CB }
+		s.ChannelMessageSendEmbed(m.ChannelID, &me)
+	} else if cmdstr[0] == "!cloaker" {
+		me := discordgo.MessageEmbed{ Title: "WHOOOOOO!", Description: "CALL ME THE CLOAKER SMOKER!", Color: 0x000000 }
+		s.ChannelMessageSendEmbed(m.ChannelID, &me)
+	} else if cmdstr[0] == "!zoltar" {
 		var greeting string = "ZOLTAR SAYS: Make your wish, !fortune."
 		s.ChannelMessageSend(m.ChannelID, greeting)
 	}
+	/*
+	for i := 0; i < 6; i++ {
+		if cmdstr[0] == commands[0] {
+			//
+		}
+	}
+	*/
 }
 
 func main() {
